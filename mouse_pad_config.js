272b9d4a19e42bd29f9265c90157638264a3d07f
jjtmutw <jjtmutw@gmail.com>
2026-05-23 14:03:37 +0800

    Update mouse_pad_config.js

---
+++ mouse_pad_config.js
@@ -0,0 +1,8 @@
+window.MOUSE_PAD_CONFIG = {
+  "mqtt": {
+    "broker": "broker.emqx.io",
+    "port": 1883,
+    "websocketUrl": "wss://broker.emqx.io:8084/mqtt",
+    "topic": "JJ/pc2/mouse/pad/cmd"
+  }
+};
