4421af071622f37487d6b6a7b0f3f3acdafd3f51
jjtmutw <jjtmutw@gmail.com>
2026-05-23 10:34:24 +0800

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'mqtt-rotary-touch-pad-v1';
+const APP_SHELL = [
+  './',
+  './rotary_knob_touch_ui.html',
+  './manifest.webmanifest',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
